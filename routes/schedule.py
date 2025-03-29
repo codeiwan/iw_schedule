8f8272ea255611a41cf7277e660e5b0eeabe5776
codeiwan <pewlv7976@gmail.com>
2025-03-29 20:44:19 +0900

    일정 추가 기능 추가

--- routes/schedule.py
+++ routes/schedule.py
@@ -2,8 +2,9 @@ import json
 
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, JSONResponse
 from pathlib import Path
+from pydantic import BaseModel
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -14,6 +15,13 @@ SCHEDULE_PATH = Path("database/schedules.json")
 def load_schedules():
     with open(SCHEDULE_PATH, "r", encoding="utf-8") as file:
         return json.load(file)
+
+""" 일정 데이터 모델 정의 """
+class ScheduleItem(BaseModel):
+    username: str
+    date: str
+    title: str
+    content: str
 
 # 스케줄 페이지 렌더링 (GET 요청)
 @router.get("/schedule")
@@ -31,3 +39,12 @@ def get_schedule(username: str, request: Request):
     schedules = load_schedules()
     user_schedules = [schedule for schedule in schedules if schedule["username"] == username]
     return templates.TemplateResponse("detail.html", {"request": request, "schedules": user_schedules, "username": username})
+
+# 일정 추가
+@router.post("/add_schedule")
+def add_schedule(schedule: ScheduleItem):
+    schedules = load_schedules()
+    schedules.append(schedule.dict())
+    with open(SCHEDULE_PATH, "w", encoding="utf-8") as file:
+        json.dump(schedules, file, ensure_ascii=False, indent=4)
+    return JSONResponse(content={"message": "일정이 추가되었습니다!"}, status_code=201)
